0d1892572de20b4a47e0f34186030ef5f8cfae7b
cn15431 <sunbin@nhn.com>
2013-06-19 13:10:36 +0800

    import flat-ui

---
+++ public/app/js/app1.js
@@ -0,0 +1,44 @@
+'use strict';
+
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', ['ui.state', 'myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+	config(['$routeProvider', '$stateProvider', function ($routeProvider, $stateProvider) {
+		$stateProvider
+			.state('index', {
+				url: "", // root route
+				views: {
+					"header": {
+						templateUrl: "partials/header.html"
+					},
+					"footer": {
+						templateUrl: "partials/footer.html"
+					}
+				}
+			})
+			.state('view1', {
+				url: "/route1",
+				views: {
+					"header": {
+						templateUrl: "partials/header.html"
+					},
+					"footer": {
+						templateUrl: "partials/footer.html"
+					}
+				}
+			})
+			.state('view2', {
+				url: "/route2",
+				views: {
+					"header": {
+						templateUrl: "partials/header.html"
+					},
+					"footer": {
+						templateUrl: "partials/footer.html"
+					}
+				}
+			})
+
+		// configure html5 to get links working on jsfiddle
+		//$locationProvider.html5Mode(true);
+	}]);
