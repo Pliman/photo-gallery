e49810c18cd169212e195f0f602f8dc2cab9d7f5
pliman <sunbin235@163.com>
2013-06-23 21:29:20 +0800

    add changelog

---
+++ test/independent-test/importer/addAlbum.js
@@ -0,0 +1,18 @@
+var ALBUM = {
+	"name": "Yinchanggou",
+	"cover": "",
+	"modifyTime": new Date().getTime()
+};
+
+//----------------------------------------------------------------
+
+var mongo = require('../../../lib/mongo');
+
+// 1. get/get-xhr /album get all album
+mongo.save('albums', ALBUM, function(err, r){
+	if(err){
+		console.log('Error saving album: ' + JSON.stringify(ALBUM));
+	}else{
+		console.log('album: ' + JSON.stringify(ALBUM) + ' saved successfully.');
+	}
+});
