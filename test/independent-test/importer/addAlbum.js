7417743e6024485a8b396081fe1d4d89c5c495d8
Pliman <sunbin@nhn.com>
2013-06-24 08:46:34 +0800

    import data.

--- test/independent-test/importer/addAlbum.js
+++ test/independent-test/importer/addAlbum.js
@@ -9,10 +9,10 @@ var ALBUM = {
 var mongo = require('../../../lib/mongo');
 
 // 1. get/get-xhr /album get all album
-mongo.save('albums', ALBUM, function(err, r){
-	if(err){
-		console.log('Error saving album: ' + JSON.stringify(ALBUM));
-	}else{
+mongo.save('albums', ALBUM, function (err, r) {
+	if (err) {
+		console.log('Error saving album: ' + JSON.stringify(ALBUM) + '\n, because of ' + err);
+	} else {
 		console.log('album: ' + JSON.stringify(ALBUM) + ' saved successfully.');
 	}
 });
