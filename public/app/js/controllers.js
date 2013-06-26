7e9e3f6d887ac40b7a5080b49106ebf239f0fdc5
Pliman <sunbin@nhn.com>
2013-06-26 15:35:30 +0800

    show loading pic.

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -3,26 +3,18 @@
 /* Controllers */
 define(['angular', './services'], function (angular) {
 	return angular.module('photo-gallery.controllers', ['photo-gallery.services']).
-		controller('index', ['$scope', 'version', function ($scope, version) {
-			$scope.scopedAppVersion = version;
-
-			$scope.photos = [
-				{ "name": "P1130053.jpg", "albumName": "Yinchanggou", "uploadTime": { "$date": 1371516455093 }, "camera": "Panasonic", "model": "DMC-LX5", "exposure": 0.004, "f": 3.2, "ISO": 80, "createTime": "2013:06:08 08:42:47", "exposureCompensation": -0.33, "_id": { "$oid": "51bfae27238f6a2416000001" } },
-				{ "name": "P1130105.jpg", "albumName": "Yinchanggou", "uploadTime": { "$date": 1371516455100 }, "camera": "Panasonic", "model": "DMC-LX5", "exposure": 0.0025, "f": 5, "ISO": 80, "createTime": "2013:06:08 09:40:15", "exposureCompensation": -0.33, "_id": { "$oid": "51bfae27238f6a2416000002" } },
-				{ "name": "P1130098.jpg", "albumName": "Yinchanggou", "uploadTime": { "$date": 1371516455102 }, "camera": "Panasonic", "model": "DMC-LX5", "exposure": 0.01, "f": 5, "ISO": 80, "createTime": "2013:06:08 09:38:13", "exposureCompensation": -0.33, "_id": { "$oid": "51bfae27238f6a2416000003" } }
-			]
+		controller('index', ['$scope', 'Photos', function ($scope, Photos) {
+			Photos.get(function(photos){
+				$scope.photos = photos;
+			});
 		}])
 		.controller('photo', ['$scope', '$stateParams', 'Photo', function ($scope, $stateParams, Photo) {
 			Photo.get({photoName: $stateParams.photoName}, function(photo) {
 				$scope.photo = photo[0];
 				$scope.photo.exposureCompensation += ' ev';
 			});
-//			console.log(Photo.get({photoName: $stateParams.photoName}));
-			//$scope.photo = { "name": "P1130053.jpg", "albumName": "Yinchanggou", "uploadTime": { "$date": 1371516455093 }, "camera": "Panasonic", "model": "DMC-LX5", "exposure": 0.004, "f": 3.2, "ISO": 80, "createTime": "2013:06:08 08:42:47", "exposureCompensation": -0.33, "_id": { "$oid": "51bfae27238f6a2416000001" } };
-			//$scope.photo = Photo.query();
 
-				// 暂时不实现photo对象传递机制，使用写死的photo对象
-			//$scope.photo.name = $stateParams.photoName;
+			// 暂时不实现photo对象传递机制，动态去获取
 			$scope.exifItems = [
 				{
 					display: 'Album Name',
--- public/app/js/services.js
+++ public/app/js/services.js
@@ -9,6 +9,11 @@ define(['angular', 'angularResource'], function (angular) {
 	angular.module('photo-gallery.services', ['ngResource'])
 		.value('version', '0.1')
 		.value('photo', '0.1')
+		.factory('Photos', function ($resource) {
+			return $resource('/photos', {}, {
+				get: {method: 'GET', isArray: true}
+			});
+		})
 		.factory('Photo', function ($resource) {
 			return $resource('/photo-data/:photoName', {}, {
 				query: {method: 'GET', params: {photoName: 'P1130053.jpg'}, isArray: true},
